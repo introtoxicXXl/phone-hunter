dc6cae0a5e6c5f3780395078a99f27e32a4df459
introtoxicXXl <minhajulabedin648@gmail.com>
2022-02-28 23:49:09 +0600

    lower case added

--- js/script.js
+++ js/script.js
@@ -2,17 +2,22 @@
 const spinner = (spinnerId) =>{
   document.getElementById('spinner').style.display = spinnerId;
 }
-
 window.addEventListener('load', function(){
   spinner('none')
 })
 
+/* --------------------------- load phone function -------------------------- */
 const loadPhone = () =>{
   const inputField = document.getElementById('search-field');
-  const inputFieldText = inputField.value;
+  const inputFieldText = inputField.value.toLowerCase();
   const url = `https://openapi.programming-hero.com/api/phones?search=${inputFieldText}`;
   inputField.value = '';
   fetch(url)
   .then(res => res.json())
-  .then(data => console.log(data))
+  .then(data => displayPhone(data))
+}
+
+/* ------------------------- display phone function ------------------------- */
+const displayPhone = (phones) =>{
+console.log(phones)
 }
