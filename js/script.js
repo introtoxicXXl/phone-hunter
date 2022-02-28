a40ac6773c0853f0361286c7b7dfd79701afecd2
introtoxicXXl <minhajulabedin648@gmail.com>
2022-03-01 04:19:34 +0600

    some code removed

--- js/script.js
+++ js/script.js
@@ -1,55 +1,54 @@
 /* --------------------------------- spinner -------------------------------- */
-const display = (proparty,value) =>{
+const display = (proparty, value) => {
   document.getElementById(proparty).style.display = value;
 }
-window.addEventListener('load', function(){
-  display('spinner','none');
+window.addEventListener('load', function () {
+  display('spinner', 'none');
 })
 
 /* --------------------------- load phone function -------------------------- */
-const loadPhone = () =>{
+const loadPhone = () => {
   const inputField = document.getElementById('search-field');
   const inputFieldText = inputField.value.toLowerCase();
-  if(inputFieldText === ''){
+  if (inputFieldText === '') {
     swal("Please Type Phone Name");
     return;
-  }else{
-    display('spinner','block');
-  const url = `https://openapi.programming-hero.com/api/phones?search=${inputFieldText}`;
-  inputField.value = '';
-  fetch(url)
-  .then(res => res.json())
-  .then(data => {
-    if(data.status){
-      displayPhone(data.data.slice(0,20));
-      
-      const resultFound = document.getElementById('result-found');
-      display('result-found','block');
-      resultFound.innerText=`${data.data.length} Result Found`;
-    }else{
-      display('spinner','block');
-      document.getElementById('show-card').textContent = '';
-      document.getElementById('show-details').textContent = '';
-      display('no-result','block');
-      display('spinner','none');
-      display('result-found','none');
-      return;
-    }
-  })
-  }
-}
+  } else {
+    display('spinner', 'block');
+    const url = `https://openapi.programming-hero.com/api/phones?search=${inputFieldText}`;
+    inputField.value = '';
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        if (data.status) {
+          displayPhone(data.data.slice(0, 20));
+          const resultFound = document.getElementById('result-found');
+          display('result-found', 'block');
+          resultFound.innerText = `${data.data.length} Result Found`;
+        } else {
+          display('spinner', 'block');
+          document.getElementById('show-card').textContent = '';
+          document.getElementById('show-details').textContent = '';
+          display('no-result', 'block');
+          display('spinner', 'none');
+          display('result-found', 'none');
+          return;
+        };
+      })
+  };
+};
 
 /* ------------------------- display phone function ------------------------- */
-const displayPhone = (phones) =>{
- const showCard = document.getElementById('show-card');
- showCard.textContent ='';
- document.getElementById('show-details').textContent = '';
- display('no-result','none')
- display('spinner','none');
- phones.forEach(phone =>{
-   const div = document.createElement('div');
-   div.className = 'col';
-   div.innerHTML =`
+const displayPhone = (phones) => {
+  const showCard = document.getElementById('show-card');
+  showCard.textContent = '';
+  document.getElementById('show-details').textContent = '';
+  display('no-result', 'none')
+  display('spinner', 'none');
+  phones.forEach(phone => {
+    const div = document.createElement('div');
+    div.className = 'col';
+    div.innerHTML = `
    <div class="card h-100 rounded-3 border-success">
         <img src="${phone.image}" class="card-img-top w-75 mx-auto p-3" alt="...">
         <div class="card-body">
@@ -59,20 +58,19 @@ const displayPhone = (phones) =>{
         </div>
       </div>
    `;
-   showCard.appendChild(div);
- })
+    showCard.appendChild(div);
+  })
 }
 /* -------------------------- load details function ------------------------- */
-const loadDetails = (details) =>{
+const loadDetails = (details) => {
   const url = `https://openapi.programming-hero.com/api/phone/${details}`;
   fetch(url)
-  .then(res => res.json())
-  .then(data => showDetails(data.data))
+    .then(res => res.json())
+    .then(data => showDetails(data.data))
 }
 
 /* -------------------------- show details function ------------------------- */
-const showDetails = (data) =>{
-  console.log(data.others)
+const showDetails = (data) => {
   const showDetails = document.getElementById('show-details');
   showDetails.innerHTML = ` 
   <div class="col d-flex justify-content-center">
@@ -149,10 +147,10 @@ const showDetails = (data) =>{
   `;
   const releaseDate = document.getElementById('release-date');
   const wlan = document.getElementById('wlan');
-  if(data.releaseDate){
+  if (data.releaseDate) {
     releaseDate.className = 'text-success';
     releaseDate.innerText = data.releaseDate;
-  }else{
+  } else {
     releaseDate.className = 'text-danger'
     releaseDate.innerText = 'Coming soon';
   };
