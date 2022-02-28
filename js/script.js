1ac4301a01abd31dacd423122863ddeb94089fff
introtoxicXXl <minhajulabedin648@gmail.com>
2022-03-01 00:18:17 +0600

    card added

--- js/script.js
+++ js/script.js
@@ -14,10 +14,26 @@ const loadPhone = () =>{
   inputField.value = '';
   fetch(url)
   .then(res => res.json())
-  .then(data => displayPhone(data))
+  .then(data => displayPhone(data.data))
 }
 
 /* ------------------------- display phone function ------------------------- */
 const displayPhone = (phones) =>{
-console.log(phones)
+ const showCard = document.getElementById('show-card');
+ phones.forEach(phone =>{
+   console.log(phone);
+   const div = document.createElement('div');
+   div.className = 'col';
+   div.innerHTML =`
+   <div class="card h-100 rounded-3 border-success">
+        <img src="${phone.image}" class="card-img-top w-75 mx-auto p-3" alt="...">
+        <div class="card-body">
+          <h3 class="card-title">${phone.phone_name}</h3>
+          <p class="fst-italic fs-6">Brand: ${phone.brand}</p>
+          <button class="btn btn-primary">Detail</button>
+        </div>
+      </div>
+   `;
+   showCard.appendChild(div);
+ })
 }
