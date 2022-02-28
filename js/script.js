894871635d0dd741a120619404d4545278284932
introtoxicXXl <minhajulabedin648@gmail.com>
2022-03-01 03:44:39 +0600

    table added

--- js/script.js
+++ js/script.js
@@ -16,18 +16,22 @@ const loadPhone = () =>{
   }else{
     display('spinner','block');
   const url = `https://openapi.programming-hero.com/api/phones?search=${inputFieldText}`;
-  // console.log(url)
   inputField.value = '';
   fetch(url)
   .then(res => res.json())
   .then(data => {
     if(data.status){
       displayPhone(data.data.slice(0,20));
+      
+      const resultFound = document.getElementById('result-found');
+      display('result-found','block');
+      resultFound.innerText=`${data.data.length} Result Found`;
     }else{
       display('spinner','block');
       document.getElementById('show-card').textContent ='';
       display('no-result','block');
       display('spinner','none');
+      display('result-found','none');
       return;
     }
   })
@@ -41,7 +45,6 @@ const displayPhone = (phones) =>{
  display('no-result','none')
  display('spinner','none');
  phones.forEach(phone =>{
-  //  console.log(phone);
    const div = document.createElement('div');
    div.className = 'col';
    div.innerHTML =`
@@ -50,10 +53,102 @@ const displayPhone = (phones) =>{
         <div class="card-body">
           <h3 class="card-title">${phone.phone_name}</h3>
           <p class="fst-italic fs-6">Brand: ${phone.brand}</p>
-          <button class="btn btn-primary">Detail</button>
+          <button onclick="loadDetails('${phone.slug}')" class="btn btn-primary">Detail</button>
         </div>
       </div>
    `;
    showCard.appendChild(div);
  })
+}
+/* -------------------------- load details function ------------------------- */
+const loadDetails = (details) =>{
+  const url = `https://openapi.programming-hero.com/api/phone/${details}`;
+  fetch(url)
+  .then(res => res.json())
+  .then(data => showDetails(data.data))
+}
+
+const showDetails = (data) =>{
+  const showDetails = document.getElementById('show-details');
+  showDetails.innerHTML = `
+  <div class="col d-flex justify-content-center align-items-center">
+          <img src="${data.image}" class="h-75" alt="">
+        </div>
+        <div class="col">
+          <table class="table table-borderless">
+            <tbody>
+              <tr>
+                <th scope="row">Name</th>
+                <td>${data.name}</td>
+              </tr>
+              <tr>
+                <th scope="row">Brand</th>
+                <td>${data.brand}</td>
+              </tr>
+              <tr>
+                <th scope="row">Release Date</th>
+                <td id="release-date"></td>
+              </tr>
+              <tr>
+                <th scope="row" class="text-center">Main Features</th>
+                <td> </td>
+              </tr>
+              <tr>
+                <th scope="row">Stroage</th>
+                <td>${data.mainFeatures.storage}</td>
+              </tr>
+              <tr>
+                <th scope="row">Display size</th>
+                <td>${data.mainFeatures.displaySize}</td>
+              </tr>
+              <tr>
+                <th scope="row">Chip set</th>
+                <td>${data.mainFeatures.chipSet}</td>
+              </tr>
+              <tr>
+                <th scope="row">Memory</th>
+                <td>${data.mainFeatures.memory}</td>
+              </tr>
+              <tr>
+                <th scope="row">Sensor</th>
+                <td>${data.mainFeatures.sensors}</td>
+              </tr>
+              <tr>
+                <th scope="row" class="text-center">Others</th>
+                <td> </td>
+              </tr>
+              <tr>
+                <th scope="row">WLAN</th>
+                <td>${data.others.WLAN}</td>
+              </tr>
+              <tr>
+                <th scope="row">Bluetooth</th>
+                <td>${data.others.Bluetooth}</td>
+              </tr>
+              <tr>
+                <th scope="row">Radio</th>
+                <td>${data.others.Radio}</td>
+              </tr>
+              <tr>
+                <th scope="row">GPS</th>
+                <td>${data.others.GPS}</td>
+              </tr>
+              <tr>
+                <th scope="row">NFC</th>
+                <td>${data.others.NFC}</td>
+              </tr>
+              <tr>
+                <th scope="row">USB</th>
+                <td>${data.others.USB}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+  `;
+  const releaseDate = document.getElementById('release-date');
+  if(data.releaseDate){
+    releaseDate.innerText = data.releaseDate;
+  }else{
+    releaseDate.innerText = 'Coming soon';
+  }
 }
